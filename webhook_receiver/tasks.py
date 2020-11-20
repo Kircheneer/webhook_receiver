f5dd85a6852e13aed3728374c77d863515afdb13
Leo Kirchner <kirchnle@uni-bremen.de>
2020-11-20 16:43:09 +0100

    Add second example task

--- webhook_receiver/tasks.py
+++ webhook_receiver/tasks.py
@@ -6,10 +6,16 @@ from webhook_receiver import celery, registry
 logging.basicConfig()
 logger = logging.getLogger(__file__)
 
-__all__ = ["create_tenant"]
+__all__ = ["create_tenant", "create_tenant_2"]
 
 
 @registry.register(model="tenant", action="create")
 @celery.task
 def create_tenant(request: Any) -> None:
     logger.warning(request)
+
+
+@registry.register(model="tenant", action="create")
+@celery.task
+def create_tenant_2(request: Any) -> None:
+    logger.error(f'Tenant {request["data"]["name"]} created.')
